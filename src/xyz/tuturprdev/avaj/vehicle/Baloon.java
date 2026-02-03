f213ca378d13dc2a1971b7d509a7550693f4ab3e
Anakoni <arthur4460011@gmail.com>
2026-02-03 12:31:36 +0100

    ADD | Printer class to print in simulation file

--- src/xyz/tuturprdev/avaj/vehicle/Baloon.java
+++ src/xyz/tuturprdev/avaj/vehicle/Baloon.java
@@ -1,5 +1,7 @@
 package xyz.tuturprdev.avaj.vehicle;
 
+import xyz.tuturprdev.avaj.parsing.Printer;
+
 public class Baloon extends Aircraft {
 
     public Baloon(long p_id, String p_name, Coordinates p_coordinates) {
@@ -11,20 +13,21 @@ public class Baloon extends Aircraft {
         switch (weatherTower.getWeather(coordinates)) {
             case "RAIN":
                 coordinates.setHeight(coordinates.getHeight() - 5);
-                System.out.printf("%s: Damn you rain! You messed up my baloon. %n", getCallsign());
+                Printer.getInstance().write(String.format("%s: Damn you rain! You messed up my baloon. %n", getCallsign()));
                 break;
             case "FOG":
                 coordinates.setHeight(coordinates.getHeight() - 3);
-                System.out.printf("%s: I can't see anything !%n", getCallsign());
+                Printer.getInstance().write(String.format("%s: I can't see anything !%n", getCallsign()));
                 break;
             case "SUN":
                 coordinates.setLongitude(coordinates.getLongitude() + 2);
                 coordinates.setHeight(coordinates.getHeight() + 4);
-                System.out.printf("%s: Let's enjoy the good weather and take some pics.%n", getCallsign());
+                Printer.getInstance().write(String.format("%s: Let's enjoy the good weather and take some pics.%n", getCallsign()));
                 break;
             case "SNOW":
                 coordinates.setHeight(coordinates.getHeight() - 15);
-                System.out.printf("%s:  It's snowing. We're gonna crash.%n", getCallsign());
+                Printer.getInstance().write(String.format("%s:  It's snowing. We're gonna crash.%n", getCallsign()));
+                break;
         }
     }
 
